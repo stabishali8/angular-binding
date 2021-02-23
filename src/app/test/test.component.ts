f39af1197116257a5d71b13ea694b8382e89f451
amirbasheer <amirbashir880@gmail.com>
2021-02-23 17:50:11 +0500

    Angular Tutorial - 10 - Template Reference Variables

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,70 +6,25 @@ import { Component, OnInit } from '@angular/core';
   <h2>
      Welcome {{name}}
   </h2>
-  <h2 class="text-special" [class]="successClass">
-     Welcome Tabish
-  </h2>
-  <h2 [class.text.danger]="hasError" [class]="">
-     Welcome Tabish
-  </h2>
-  <h2 [ngClass]="messageClasses">
-    Degree Vs Skills
-  </h2>
-
-  <h2 [style.color]="hasError ? 'red' : 'green'">
-  Style Binding</h2>
-
-  <h2 [style.color]="highlightColor">
-  Style Binding</h2>
-
-  <h2 [ngStyle]="titleStyle">
-  Style Binding</h2>
-
-  <button (click)="onClick($event)">Greet</button>
-  <button (click)="onClick($event)">Greet</button>
-
-  {{greeting}}
+  <input #myInput type="text">
+  <button (click)="logMessage(myInput.value)">
+    Log
+  </button>
   `
   
   ,
-  styles: [`
-    .text-success {
-      color : green;
-    }
-    .text-danger{
-      color: red;
-    }
-    text-special{
-      font-style: italic;
-    }
-  `]
+  styles: []
 })
 export class TestComponent implements OnInit {
 
   public name = "Syed Tabish Ali";
-  public myId = "testId"
-  public successClass = "text-success";
-  public hasError = true;
-  public isSpecial = "true";
-  public highlightColor = "orange";
-  public greeting = "";
-  public messageClasses = {
-    "text-success": !this.hasError,
-    "text-danger":this.hasError,
-    "text-special":this.isSpecial
-  }
-
-  public titleStyle = {
-    fontStyle:"Italic"
-  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onClick(event:any){
-    console.log(event)
-    this.greeting = event.type
+  logMessage(value:string){
+    console.log(value)
   }
-
 }
