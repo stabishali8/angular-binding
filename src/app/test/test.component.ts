3934a83383acdc57c7d02a6a3b1d13aed6c6b33d
amirbasheer <amirbashir880@gmail.com>
2021-02-23 16:09:11 +0500

    Angular Tutorial - 7 - Class Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,14 +6,40 @@ import { Component, OnInit } from '@angular/core';
   <h2>
      Welcome {{name}}
   </h2>
-  <input [id]="myId" type="text" value="tabish"/>
+  <h2 class="text-special" [class]="successClass">
+     Welcome Tabish
+  </h2>
+  <h2 [class.text.danger]="hasError" [class]="">
+     Welcome Tabish
+  </h2>
+  <h2 [ngClass]="messageClasses">
+    Degree Vs Skills
+  </h2>
   `,
-  styles: []
+  styles: [`
+    .text-success {
+      color : green;
+    }
+    .text-danger{
+      color: red;
+    }
+    text-special{
+      font-style: italic;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
   public name = "Syed Tabish Ali";
   public myId = "testId"
+  public successClass = "text-success";
+  public hasError = false;
+  public isSpecial = "true";
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger":this.hasError,
+    "text-special":this.isSpecial
+  }
   constructor() { }
 
   ngOnInit(): void {
